5046798f949f09ab3f079f957d2def8e6ff8e6b2
RobertSabiryanov <robert.sabiryanov@gmail.com>
2015-09-14 01:26:21 +0500

    example folder

---
+++ example/logic.js
@@ -0,0 +1,16 @@
+/**
+ * Created by RobertSabiryanov on 11.09.15.
+ */
+class Logic{
+    constructor(provider, logger){
+        this.provider= provider;
+        this.logger=logger;
+    }
+    getReversedData(){
+        this.logger.log('getReversedData');
+        return this.provider.getData().reverse();
+    }
+}
+Logic.$inject=['provider', 'logger'];
+
+export default {Logic};
