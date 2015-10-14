d0b80e5e83f6da35f4ea992c20495caebde0936f
Денис <denis@MacBook-Pro-Denis-2.local>
2015-10-14 14:01:13 +0500

    fix build script

--- dist/ioc.js
+++ dist/ioc.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['exports'], factory);
-    } else if (typeof exports !== 'undefined') {
-        factory(exports);
+        define(['exports', 'module'], factory);
+    } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+        factory(exports, module);
     } else {
         var mod = {
             exports: {}
         };
-        factory(mod.exports);
+        factory(mod.exports, mod);
         global.ioc = mod.exports;
     }
-})(this, function (exports) {
+})(this, function (exports, module) {
     'use strict';
 
     var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
@@ -120,5 +120,5 @@
         return Ioc;
     })();
 
-    exports['default'] = Ioc;
+    module.exports = Ioc;
 });
